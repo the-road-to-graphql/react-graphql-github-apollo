c95c49997bf5d7d8a0ddd5afbcbab90926831cc8
Robin Wieruch <wrobin@gmx.net>
2018-02-04 12:10:36 +0100

    ticking Loading component

--- src/Loading/index.js
+++ src/Loading/index.js
@@ -1,19 +1,46 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './style.css';
 
-const Loading = ({ isCenter }) => {
-  const classNames = ['Loading'];
+const TICK_RATE = 500;
 
-  if (isCenter) {
-    classNames.push('Loading_center');
+class Loading extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dots: 0,
+    };
   }
 
-  return (
-    <div className={classNames.join(' ')}>
-      <small>Loading ...</small>
-    </div>
-  );
+  componentDidMount() {
+    this.interval = setInterval(this.onTick, TICK_RATE);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  onTick = () => {
+    this.setState(prevState => ({ dots: (prevState.dots + 1) % 4 }));
+  }
+
+  render() {
+    const { isCenter } = this.props;
+    const { dots } = this.state;
+
+    const classNames = ['Loading'];
+
+    if (isCenter) {
+      classNames.push('Loading_center');
+    }
+
+    return (
+      <div className={classNames.join(' ')}>
+        <small>Loading {new Array(dots).fill(0).map(dot => `.`)}</small>
+      </div>
+    );
+  }
 }
 
 export default Loading;
