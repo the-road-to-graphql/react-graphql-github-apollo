cdc862c9dd55689915c331c90b053c1cbb930648
Robin Wieruch <wrobin@gmx.net>
2018-03-23 14:40:02 +0800

    refactor Issues to use render props

--- src/Issues/index.js
+++ src/Issues/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { graphql, withApollo } from 'react-apollo';
+import { Query, withApollo } from 'react-apollo';
 import gql from 'graphql-tag';
 import { compose, withState } from 'recompose';
 
@@ -90,7 +90,6 @@ const Issues = ({
   repositoryName,
   showState,
   onChangeShowState,
-  data,
   client,
 }) => (
   <div className="Issues">
@@ -111,58 +110,67 @@ const Issues = ({
         showState={showState}
         repositoryOwner={repositoryOwner}
         repositoryName={repositoryName}
-        data={data}
       />
     )}
   </div>
 );
 
-const IssuesList = ({
-  showState,
-  repositoryOwner,
-  repositoryName,
-  data: { error, loading, repository, fetchMore },
-}) => {
-  if (loading) {
-    return <Loading />;
-  }
+const IssuesList = ({ showState, repositoryOwner, repositoryName }) => (
+  <Query
+    query={ISSUES_OF_REPOSITORY}
+    variables={{
+      cursor: null,
+      repositoryOwner,
+      repositoryName,
+      kindOfIssue: KIND_OF_ISSUES[showState],
+    }}
+    skip={!isShow(showState)}
+  >
+    {({ data, loading, error, fetchMore }) => {
+      const { repository } = data;
 
-  if (error) {
-    return <ErrorMessage error={error} />;
-  }
+      if (loading) {
+        return <Loading />;
+      }
 
-  return (
-    <div className="IssueList">
-      {repository.issues.edges.length ? (
-        <div>
-          {repository.issues.edges.map(issue => (
-            <Issue
-              key={issue.node.id}
-              issue={issue.node}
-              repositoryOwner={repositoryOwner}
-              repositoryName={repositoryName}
-            />
-          ))}
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
 
-          <FetchMore
-            payload={{
-              repositoryOwner,
-              repositoryName,
-              showState,
-            }}
-            loading={loading}
-            pageInfo={repository.issues.pageInfo}
-            doFetchMore={doFetchMore(fetchMore)}
-          >
-            Issues
-          </FetchMore>
+      return (
+        <div className="IssueList">
+          {repository.issues.edges.length ? (
+            <div>
+              {repository.issues.edges.map(issue => (
+                <Issue
+                  key={issue.node.id}
+                  issue={issue.node}
+                  repositoryOwner={repositoryOwner}
+                  repositoryName={repositoryName}
+                />
+              ))}
+
+              <FetchMore
+                payload={{
+                  repositoryOwner,
+                  repositoryName,
+                  showState,
+                }}
+                loading={loading}
+                pageInfo={repository.issues.pageInfo}
+                doFetchMore={doFetchMore(fetchMore)}
+              >
+                Issues
+              </FetchMore>
+            </div>
+          ) : (
+            <div>No issues ...</div>
+          )}
         </div>
-      ) : (
-        <div>No issues ...</div>
-      )}
-    </div>
-  );
-};
+      );
+    }}
+  </Query>
+);
 
 const ISSUES_OF_REPOSITORY = gql`
   query(
@@ -191,20 +199,7 @@ const ISSUES_OF_REPOSITORY = gql`
   }
 `;
 
-const ISSUES_OF_REPOSITORY_CONFIG = {
-  options: ({ repositoryOwner, repositoryName, showState }) => ({
-    variables: {
-      cursor: null,
-      repositoryOwner,
-      repositoryName,
-      kindOfIssue: KIND_OF_ISSUES[showState],
-    },
-    skip: !isShow(showState),
-  }),
-};
-
 export default compose(
   withState('showState', 'onChangeShowState', SHOW_STATES.NO_ISSUES),
-  graphql(ISSUES_OF_REPOSITORY, ISSUES_OF_REPOSITORY_CONFIG),
   withApollo,
 )(Issues);
