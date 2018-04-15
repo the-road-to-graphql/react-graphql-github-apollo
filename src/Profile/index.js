8317a67cb93d88879b8591531956f5ff09a770a8
Robin Wieruch <wrobin@gmx.net>
2018-04-15 16:22:54 +0800

    chapter 6: query (chapter 5 only HOC refactor)

--- src/Profile/index.js
+++ src/Profile/index.js
@@ -2,16 +2,13 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Query } from 'react-apollo';
 
-// import { GET_REPOSITORIES_OF_CURRENT_USER } from './queries';
 import RepositoryList from '../Repository';
 import Loading from '../Loading';
-// import ErrorMessage from '../Error';
+import ErrorMessage from '../Error';
 
 export const GET_REPOSITORIES_OF_CURRENT_USER = gql`
   {
     viewer {
-      login
-      name
       repositories(
         first: 5
         orderBy: { direction: DESC, field: STARGAZERS }
@@ -46,7 +43,11 @@ export const GET_REPOSITORIES_OF_CURRENT_USER = gql`
 
 const Profile = () => (
   <Query query={GET_REPOSITORIES_OF_CURRENT_USER}>
-    {({ data, loading }) => {
+    {({ data, loading, error }) => {
+      if (error) {
+        return <ErrorMessage error={error} />;
+      }
+
       const { viewer } = data;
 
       if (loading || !viewer) {
--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { ApolloClient } from 'apollo-client';
+import { ApolloLink } from 'apollo-link';
 import { HttpLink } from 'apollo-link-http';
+import { onError } from 'apollo-link-error';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
 import './style.css';
@@ -21,10 +23,23 @@ const httpLink = new HttpLink({
   },
 });
 
+const errorLink = onError(({ graphQLErrors, networkError }) => {
+  if (graphQLErrors)
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+
+  if (networkError) console.log(`[Network error]: ${networkError}`);
+});
+
+const link = ApolloLink.from([errorLink, httpLink]);
+
 const cache = new InMemoryCache();
 
 const client = new ApolloClient({
-  link: httpLink,
+  link,
   cache,
 });
 
